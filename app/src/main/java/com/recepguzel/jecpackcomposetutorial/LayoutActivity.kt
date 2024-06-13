141508172bd63bdda10eaf5d5bdf3e7800ee7a55
Recep Güzel <recepgzelr@gmail.com>
2024-06-13 18:11:50 +0300

    start project <3

---
+++ app/src/main/java/com/recepguzel/jecpackcomposetutorial/LayoutActivity.kt
@@ -0,0 +1,96 @@
+package com.recepguzel.jecpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.recepguzel.jecpackcomposetutorial.ui.theme.JecpackComposeTutorialTheme
+
+class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JecpackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnRowColumnBox()
+                }
+            }
+        }
+    }
+}
+/*
+ColumnLayout -> LinearLayout Vertical
+RowLayout -> LinearLayout Horizontal
+BoxLayout -> Card view
+ */
+
+@Composable
+fun LearnRowColumnBox() {
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello Column 1")
+        Text(text = "Hello Column 2")
+
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello Column 1")
+        Text(text = "Hello Column 2")
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(modifier = Modifier
+            .height(300.dp)
+            .width(300.dp)
+            .background(Color.Green)) {
+            Text(
+                text = "Hello Box",
+                modifier = Modifier.align(Alignment.Center),
+                Color.Blue,
+                fontStyle = FontStyle.Italic,
+                fontSize = 30.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowPreviewLayout() {
+    JecpackComposeTutorialTheme {
+        LearnRowColumnBox()
+    }
+}
---
+++ app/src/main/java/com/recepguzel/jecpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.recepguzel.jecpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.recepguzel.jecpackcomposetutorial.ui.theme.JecpackComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JecpackComposeTutorialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearnTextAndModifiers()
+                }
+            }
+        }
+    }
+}
+/*
+Text Compponentinde text özellikleri ve modifier vardır .
+Modifier alt özellikleri bulunur  padding,background,click işlemi gibi !
+ */
+@Composable
+fun LearnTextAndModifiers(){
+
+    val clickOnText = {}
+    Text(text = stringResource(id = R.string.hello_text),
+        color = Color.Black,
+        fontSize = 32.sp,
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier
+            .padding(22.dp)
+            .background(Color.Blue)
+            .clickable(onClick = clickOnText) )
+}
+@Preview(showBackground = true)
+@Composable
+fun ShowPreview(){
+    JecpackComposeTutorialTheme{
+        LearnTextAndModifiers()
+    }
+}
